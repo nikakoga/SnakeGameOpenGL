ff5760623fdc8a9c04632be2c2cb9dff306bbef4
Weronika Koga <weronikakoga@gmail.com>
2024-05-29 19:29:47 +0200

    square at random places

--- SNAKE/main.cpp
+++ SNAKE/main.cpp
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#define GRID_COUNT_X 8
+#define GRID_COUNT_Y 8
 
 GLfloat verticles[] = {
     0.0, 0.0,
@@ -13,46 +17,130 @@ GLfloat verticles[] = {
 
 GLuint indices[] = {
     0, 1, 2,
-    3, 0, 1
+    2, 3, 0
 };
 
 const GLchar* vShader_Code =
 "#version 330 core\n"
-"layout (location =0) in vec2 v_pos;\n"
-"void main(){\n"
-"gl_Position = vec4(v_pos, 0.0 1.0);\n"
+"layout (location = 0) in vec2 v_pos;\n"
+"uniform float POSITION;\n"
+"uniform vec2 GRID_COUNT;\n"
+"void main() {\n"
+"   vec2 GRID_SIZE = vec2(2.0 / GRID_COUNT.x, 2.0 / GRID_COUNT.y);\n"
+"   vec2 pos_center = vec2((mod(POSITION, GRID_COUNT.x) * GRID_SIZE.x) - 1.0 + (GRID_SIZE.x) / 2.0,\n"
+"                          1.0 - GRID_SIZE.y - (floor(POSITION / GRID_COUNT.x) * GRID_SIZE.y) + (GRID_SIZE.y / 2.0));\n"
+"   gl_Position = vec4(\n"
+"       pos_center.x - (GRID_SIZE.x / 2.0) + (v_pos.x * GRID_SIZE.x),\n"
+"       pos_center.y - (GRID_SIZE.y / 2.0) + (v_pos.y * GRID_SIZE.y),\n"
+"       0.0, 1.0\n"
+"   );\n"
 "}\0";
 
 const GLchar* fShader_Code =
 "#version 330 core\n"
 "out vec4 fragment_color;\n"
-"void main(){\n"
-"fragment_color = vec4(1.0,0.5,0.5,1.0);\n"
+"void main() {\n"
+"   fragment_color = vec4(1.0, 0.5, 0.5, 1.0);\n"
 "}\0";
 
-//void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
-//    if (action == GLFW_PRESS) {
-//        //if (key == GLFW_KEY_LEFT) speed_x = -PI / 2;
-//        //if (key == GLFW_KEY_RIGHT) speed_x = PI / 2;
-//        //if (key == GLFW_KEY_UP) speed_y = PI / 2;
-//        //if (key == GLFW_KEY_DOWN) speed_y = -PI / 2;
-//    }
-//    if (action == GLFW_RELEASE) {
-//        //if (key == GLFW_KEY_LEFT) speed_x = 0;
-//        //if (key == GLFW_KEY_RIGHT) speed_x = 0;
-//        //if (key == GLFW_KEY_UP) speed_y = 0;
-//        //if (key == GLFW_KEY_DOWN) speed_y = 0;
-//    }
-//}
+struct Button
+{
+    GLboolean down, last, pressed;
+};
+struct Keyboard
+{
+    struct Button keys[GLFW_KEY_LAST];
+};
+struct Keyboard keyboard = {};
 
-void initOpenGLProgram(GLFWwindow* window) {
-    //************Tutaj umieszczaj kod, który nale¿y wykonaæ raz, na pocz¹tku programu************
-    glClearColor(0, 1, 0, 1);
-    glEnable(GL_DEPTH_TEST);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Wyczyœæ bufor koloru i g³êbokoœci
-    //glfwSetWindowSizeCallback(window, windowResizeCallback);
-    //glfwSetKeyCallback(window, keyCallback);
+enum Directions
+{
+    LEFT = 1,
+    RIGHT = 2,
+    UP = 4,
+    DOWN = 8,
+    PAUSE = 16
+};
+
+struct Snake
+{
+    GLuint counter;
+    GLuint positions[GRID_COUNT_X * GRID_COUNT_Y];
+    GLuint direction;
+};
+
+struct Snake snake = {};
+GLuint foodPosition = 0;
+
+GLuint generateRandomPosition(void)
+{
+    GLuint randomPosition = (GLuint)(rand() % (GRID_COUNT_X * GRID_COUNT_Y));
+    GLboolean free_position;
+    do {
+        free_position = GL_TRUE;
+        for (GLuint i = 0; i < snake.counter; i++)
+        {
+            if (snake.positions[i] == randomPosition)
+            {
+                free_position = GL_FALSE;
+                randomPosition++;
+                if (randomPosition >= (GRID_COUNT_X * GRID_COUNT_Y))
+                {
+                    randomPosition %= (GRID_COUNT_X * GRID_COUNT_Y);
+                }
+            }
+        }
+    } while (!free_position);
+    return randomPosition;
 }
+
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+
+    if (key < 0)
+    {
+        return;
+    }
+
+    switch (action)
+    {
+    case GLFW_PRESS:
+        keyboard.keys[key].down = GL_TRUE;
+        break;
+    case GLFW_RELEASE:
+        keyboard.keys[key].down = GL_FALSE;
+        break;
+    default:
+        break;
+    }
+}
+
+void _button_array_update(struct Button* buttons)
+{
+    for (int i = 0; i < GLFW_KEY_LAST; i++)
+    {
+        buttons[i].pressed = buttons[i].down && !buttons[i].last;
+        buttons[i].last = buttons[i].down;
+    }
+}
+
+void keyFunctions(GLFWwindow* window)
+{
+    _button_array_update(keyboard.keys);
+
+    if (keyboard.keys[GLFW_KEY_ESCAPE].down)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
+//void initOpenGLProgram(GLFWwindow* window) {
+//    //************Tutaj umieszczaj kod, który nale¿y wykonaæ raz, na pocz¹tku programu************
+//    glClearColor(0, 1, 0, 1);
+//    glEnable(GL_DEPTH_TEST);
+//    keyFunctions(window);
+//
+//    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Wyczyœæ bufor koloru i g³êbokoœci
+//    //glfwSetWindowSizeCallback(window, windowResizeCallback);
+//    //glfwSetKeyCallback(window, keyCallback);
+//}
 
 void freeOpenGLProgram(GLFWwindow* window) {
     //************Tutaj umieszczaj kod, który nale¿y wykonaæ po zakoñczeniu pêtli g³ównej************
@@ -60,6 +148,7 @@ void freeOpenGLProgram(GLFWwindow* window) {
 
 int main(int argc, char const* argv[]) {
 
+    srand(time(NULL));
     if (!glfwInit()) {
         fprintf(stderr, "ERROR: Failed to initialize GLFW!\n");
         exit(EXIT_FAILURE);
@@ -74,6 +163,7 @@ int main(int argc, char const* argv[]) {
     }
 
     glfwMakeContextCurrent(window); // Od tego momentu kontekst okna staje siê aktywny i polecenia OpenGL bêd¹ dotyczyæ w³aœnie jego.
+    glfwSetKeyCallback(window, keyCallback);
     glfwSwapInterval(1); // Czekaj na 1 powrót plamki przed pokazaniem ukrytego bufora
 
     // Inicjalizacja GLAD
@@ -116,19 +206,29 @@ int main(int argc, char const* argv[]) {
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(GLfloat), 0);
     glEnableVertexAttribArray(0);
 
+    snake.counter = 1;
+    snake.positions[0] = (GLuint)(rand() % (GRID_COUNT_X * GRID_COUNT_Y));
+    snake.direction = PAUSE;
+
+    foodPosition = generateRandomPosition();
 
     while (!glfwWindowShouldClose(window)) { // Tak d³ugo jak okno nie powinno zostaæ zamkniête
 
-        initOpenGLProgram(window); // Operacje inicjuj¹ce
+        //initOpenGLProgram(window); // Operacje inicjuj¹ce
+        glClearColor(0, 1, 0, 1);
+        glEnable(GL_DEPTH_TEST);
+        keyFunctions(window);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Wyczyœæ bufor koloru i g³êbokoœci
 
         glBindVertexArray(VAO);
         glUseProgram(shader_programm);
+        glUniform1f(glGetUniformLocation(shader_programm, "POSITION"), snake.positions[0]);
+        glUniform2f(glGetUniformLocation(shader_programm, "GRID_COUNT"), (GLfloat)GRID_COUNT_X, (GLfloat)GRID_COUNT_Y);
 
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-        //drawScene(window, angle_x, angle_y); // Wykonaj procedurê rysuj¹c¹
 
-        glfwSwapBuffers(window); // Zamieñ bufory
         glfwPollEvents(); // Wykonaj procedury callback w zale¿noœci od zdarzeñ jakie zasz³y
+        glfwSwapBuffers(window); // Zamieñ bufory
     }
 
     freeOpenGLProgram(window);
@@ -136,8 +236,4 @@ int main(int argc, char const* argv[]) {
     glfwDestroyWindow(window); // Usuñ kontekst OpenGL i okno
     glfwTerminate(); // Zwolnij zasoby zajête przez GLFW
     exit(EXIT_SUCCESS);
-
-    //#ifdef __APPLE__
-    //    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
-    //#endif
 }
